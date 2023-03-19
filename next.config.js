65e7531c3b96e022b427901b81c0b3aa19e8a853
Kojiro <johnjiro1114@gmail.com>
2023-03-19 20:58:26 +0900

    initial

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/kjr020',
+  assetPrefix: '/kjr020/',
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
